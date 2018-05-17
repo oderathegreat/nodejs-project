6278c60730f4dd7b95b8a800ebfef55a0570451d
Ted <ted@redpulse.co.ke>
2018-05-17 05:09:05 +0300

    slight modificationss

--- serve.js
+++ serve.js
@@ -1,15 +1,14 @@
 var express = require ('express');
 var app = express();
+var mangoose = require('mangoose');
+
 
 app.get('/', function(request, response) {
 
-response.Send('Working Perfetcly');
+response.Send('API working ');
 
 });
 
 app.listen(8000);
-
-
-
 // Print URL for accessing server
 console.log('API is running on port 8000');
